ba77d6d0529d4dcd6d3e7dbe8c74b599a3bd3d77
Steven Miller <msm@gatech.edu>
2014-07-17 17:47:11 -0400

    Start PEP8 edits to library

--- backloggery_puller.py
+++ backloggery_puller.py
@@ -5,32 +5,41 @@ from string import Template
 from HTMLParser import HTMLParser
 
 
-
 class GamesRequest:
-   """This class requests pages from the backloggery site.
+    """This class requests pages from the backloggery site.
 
-   Using a spoofed header this class pulls games lists in html form from
-   a given backloggery page. The only thing it takes as a parameter is a username
-   so an object must be made for every username you want to make requests for.
-   """
-   _spoofed_header = {
-   'Connection': 'keep-alive', 
-   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36',
-   'Accept': '*/*',
-   'Referer': '',
-   'Accept-Encoding': 'gzip,deflate,sdch',
-   'Accept-Language': 'en-US,en;q=0.8'
-   }
-   _page_request_template = Template('http://backloggery.com/ajax_moregames.php?user=$username')
-   _referer_template = Template('http://backloggery.com/games.php?user=$username')
-   _more_games_template = Template('&console=&rating=&status=&unplayed=&own=&search=&comments=&region=&region_u=0&wish=&alpha=&temp_sys=ZZZ&total=0&aid=1&ajid=$entries')
-   _request_footer = '\t<div id="output2">'   
-   
-   def __init__(self, username):
-      """Construct the Request object for a specific user"""
-      self.username = username
-      self.request_url = self._page_request_template.substitute(username=self.username)
-      self._spoofed_header['Referer'] = self._referer_template.substitute(username=self.username)
+    Using a spoofed header this class pulls games lists in html form from
+    a given backloggery page. The only thing it takes as a parameter is a
+    username so an object must be made for every username you want to make
+    requests for.
+    """
+
+    _spoofed_header = {
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+        '(KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36',
+        'Accept': '*/*',
+        'Referer': '',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Accept-Language': 'en-US,en;q=0.8'
+    }
+    _page_request_template = Template('http://backloggery.com/ajax_moregames.'
+                                      'php?user=$username')
+    _referer_template = Template('http://backloggery.com/games.php?'
+                                 'user=$username')
+    _more_games_template = Template('&console=&rating=&status=&unplayed=&own='
+                                    '&search=&comments=&region=&region_u=0'
+                                    '&wish=&alpha=&temp_sys=ZZZ&total=0&aid=1'
+                                    '&ajid=$entries')
+    _request_footer = '\t<div id="output2">'
+
+    def __init__(self, username):
+        """Construct the Request object for a specific user"""
+        self.username = username
+        self.request_url = self._page_request_template.substitute(
+            username=self.username)
+        self._spoofed_header['Referer'] = self._referer_template.substitute(
+            username=self.username)
       
    def more_games(self, start_point):
       """Perform a call to the backloggery 'more_games' AJAX call to get 50 more games from a specified start point"""
